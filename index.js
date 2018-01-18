f3edc231ff2ab9350ce482ab8f9f0054eafbdce5
Kacper Kozak <kacper.kozak@siili.com>
2018-01-18 14:44:33 +0100

    Init

---
+++ index.js
@@ -0,0 +1,34 @@
+const inputEl = document.getElementById('text');
+const statusEl = document.getElementById('status');
+
+const value$ = Rx.Observable.fromEvent(inputEl, 'keyup').map(event => event.target.value);
+const startSave$ = value$.debounceTime(1000).distinctUntilChanged();
+
+var pauser$ = new Rx.Subject();
+console.log();
+
+const saveRequest$ = startSave$.switchMap(data =>
+    Rx.Observable.timer(1000)
+        .mapTo(data)
+        .takeUntil(value$),
+);
+
+const isSaved$ = Rx.Observable.combineLatest(
+    value$,
+    startSave$.startWith(null),
+    saveRequest$.startWith(null),
+)
+    .map(([value, started, saved]) => {
+        if (value === saved) return 2;
+        if (value === started) return 1;
+        return 0;
+    })
+    .distinctUntilChanged();
+
+isSaved$.subscribe(status => {
+    statusEl.innerText = ['✘', '…', '✔'][status];
+});
+
+saveRequest$.subscribe(data => {
+    console.log('!!! Save request - ', data);
+});
