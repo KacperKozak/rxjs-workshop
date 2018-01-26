ac1d130e80c045e5a3a34d2ea818b258eb063e66
Kacper Kozak <kacper.kozak@siili.com>
2018-01-26 11:37:33 +0100

    #1 task refactoring

--- 1-drag-and-drop/index.js
+++ 1-drag-and-drop/index.js
@@ -1,21 +1,14 @@
 const circleEl = document.getElementById('circle');
 const circle2El = document.getElementById('circle2');
 
-// Get the three major events
-const mousedown$ = Rx.Observable.fromEvent(circleEl, 'mousedown');
 const mousemove$ = Rx.Observable.fromEvent(document, 'mousemove');
-const mouseup$ = Rx.Observable.fromEvent(document, 'mouseup');
 
-const mousedrag$ = mousedown$.switchMap(md =>
-    mousemove$
-        .map(mm => ({
-            left: mm.clientX,
-            top: mm.clientY,
-        }))
-        .takeUntil(mouseup$),
-);
+const mousedrag$ = mousemove$.map(mm => ({
+    left: mm.clientX,
+    top: mm.clientY,
+}));
 
-mousedrag$.subscribe(function(pos) {
+mousedrag$.subscribe(pos => {
     circleEl.style.top = pos.top + 'px';
     circleEl.style.left = pos.left + 'px';
 });
@@ -23,7 +16,7 @@ mousedrag$.subscribe(function(pos) {
 mousedrag$
     .delay(100)
     .map(pos => ({ left: pos.left + 50, top: pos.top }))
-    .subscribe(function(pos) {
+    .subscribe(pos => {
         circle2El.style.top = pos.top + 'px';
         circle2El.style.left = pos.left + 'px';
     });
