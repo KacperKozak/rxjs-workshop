1bd8e4c720b82cb654a52c6ae65f3ad2c426eb1a
Kacper Kozak <kacper.kozak@siili.com>
2018-01-26 11:49:15 +0100

    Removed comment

--- 1-drag-and-drop/index.js
+++ 1-drag-and-drop/index.js
@@ -1,7 +1,6 @@
 const circleEl = document.getElementById('circle');
 const circle2El = document.getElementById('circle2');
 
-// Get the three major events
 const mousedown$ = Rx.Observable.fromEvent(circleEl, 'mousedown');
 const mousemove$ = Rx.Observable.fromEvent(document, 'mousemove');
 const mouseup$ = Rx.Observable.fromEvent(document, 'mouseup');
