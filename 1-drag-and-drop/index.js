028a41992a33b7de583f75d1a29d74c83af50578
Kacper Kozak <kacper.kozak@siili.com>
2018-03-15 17:31:36 +0100

    Removed solutions

--- 1-drag-and-drop/index.js
+++ 1-drag-and-drop/index.js
@@ -1,28 +1,6 @@
-const circleEl = document.getElementById('circle');
-const circle2El = document.getElementById('circle2');
+const circleEl = document.getElementById("circle");
+const circle2El = document.getElementById("circle2");
 
-const mousedown$ = Rx.Observable.fromEvent(circleEl, 'mousedown');
-const mousemove$ = Rx.Observable.fromEvent(document, 'mousemove');
-const mouseup$ = Rx.Observable.fromEvent(document, 'mouseup');
-
-const mousedrag$ = mousedown$.switchMap(md =>
-    mousemove$
-        .map(mm => ({
-            left: mm.clientX,
-            top: mm.clientY,
-        }))
-        .takeUntil(mouseup$),
-);
-
-mousedrag$.subscribe(pos => {
-    circleEl.style.top = pos.top + 'px';
-    circleEl.style.left = pos.left + 'px';
-});
-
-mousedrag$
-    .delay(100)
-    .map(pos => ({ left: pos.left + 50, top: pos.top }))
-    .subscribe(pos => {
-        circle2El.style.top = pos.top + 'px';
-        circle2El.style.left = pos.left + 'px';
-    });
+const mousedown$ = Rx.Observable.fromEvent(circleEl, "mousedown");
+const mousemove$ = Rx.Observable.fromEvent(document, "mousemove");
+const mouseup$ = Rx.Observable.fromEvent(document, "mouseup");
--- 3-react-redux-observable/src/epics.js
+++ 3-react-redux-observable/src/epics.js
@@ -1,21 +1,9 @@
-import { combineEpics } from 'redux-observable';
-import Rx from 'rxjs';
-import { results, loading } from './appActions';
+import { combineEpics } from "redux-observable";
+import Rx from "rxjs";
+import { results, loading } from "./appActions";
 
-const URL = 'https://api.punkapi.com/v2/beers?beer_name=';
+const URL = "https://api.punkapi.com/v2/beers?beer_name=";
 
-const searchEpic = action$ =>
-    action$
-        .ofType('SEARCH')
-        .filter(action => action.payload !== '')
-        .debounceTime(1000)
-        .switchMap(action =>
-            Rx.Observable.ajax(URL + action.payload)
-                .map(({ response }) => results(response))
-                .takeUntil(action$.ofType('SEARCH'))
-                .startWith(loading(true)),
-        );
+const searchEpic = action$ => null;
 
-const loadingEndEpic = action$ => action$.ofType('RESULTS').mapTo(loading(false));
-
-export default combineEpics(searchEpic, loadingEndEpic);
+export default searchEpic;
